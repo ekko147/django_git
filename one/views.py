e8a3809fd85a43d48afbaa52521958dd000b9e46
ekko <1178219442@qq.com>
2020-06-26 22:38:26 +0800

    这是新建app并修改文件后的第一次提交

--- one/views.py
+++ one/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponse
 
 # Create your views here.
+def index(request):
+    print('第一次')
+    return HttpResponse('第一次')
